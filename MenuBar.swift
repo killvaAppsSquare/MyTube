95587d58ddcb983d8fd8532381856274ddd81d85
killvaAppsSquare <killvappssquare@gmail.com>
2017-08-01 17:54:04 +0200

    first commit

---
+++ MenuBar.swift
@@ -0,0 +1,21 @@
+//
+//  MenuBar.swift
+//  Youtube
+//
+//  Created by Macbook Pro on 8/1/17.
+//  Copyright © 2017 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class MenuBar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Youtube/Helper.swift
@@ -0,0 +1,9 @@
+//
+//  Helper.swift
+//  Youtube
+//
+//  Created by Macbook Pro on 8/1/17.
+//  Copyright © 2017 Macbook Pro. All rights reserved.
+//
+
+import Foundation
